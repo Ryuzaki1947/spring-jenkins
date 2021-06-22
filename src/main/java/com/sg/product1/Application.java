1f89091768ff89acd92b0fc9b3621197c7758b6f
Ryuzaki1947 <kkumar@ce.iitr.ac.in>
2021-06-22 16:25:34 +0530

    first commit

---
+++ src/main/java/com/sg/product1/Application.java
@@ -0,0 +1,17 @@
+package com.sg.product1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application { 
+	static final Logger logger=LoggerFactory.getLogger(Application.class);
+	public static void main(String [] args) {
+//		logger.info("HEY2");
+		logger.info("info msg");
+		logger.error("error msg");
+		logger.warn("warn msg");
+		logger.debug("debug msg");
+		System.out.println("Hey");
+	}
+
+}
